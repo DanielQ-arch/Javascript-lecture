2106d6c623d4f4c089fb3f6822fb5df662691d77
DanielQ-arch <drq@njit.edu>
2024-11-12 12:34:37 -0500

    update

--- scriptOne.js
+++ scriptOne.js
@@ -6,3 +6,13 @@ let userName ="Harry";
 
 userName = "Potter"
 console.log(userName);
+
+let FirstName = "Daniel";
+console.log(FirstName);
+
+let LastName = "Quisbert";
+console.log(LastName);
+
+const valuePi = 3.14;
+
+console.log(valuePi);
