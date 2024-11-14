83afd0e87ededb3307a086e694b8b159e61b029a
DanielQ-arch <drq@njit.edu>
2024-11-14 15:58:17 -0500

    update

--- scriptOne.js
+++ scriptOne.js
@@ -10,6 +10,48 @@ let LastName = "Quisbert";
 console.log(LastName);
 
 
+var name = "Harry Potter";
+console.log(String);
+
+var age = 1000;
+console.log(Number);
+
+var Njit = true;
+console.log(Boolean)
+
+var Password;
+console.log(undefined)
+
+var RollNumber = 15;
+
+if (RollNumber > 10){
+    console.log("The rollnumer is greater than 10")
+}
+
+var isMember = true;
+
+if (isMember = true){
+    console.log("Member discount applied")
+}
+
+var startValue = 50;
+
+if (startValue > 0){
+    console.log("The startValue is positive.")
+}
+
+else if (startValue < 0){
+    console.log("The startValue is negative.")
+}
+
+else{
+    console.log("The startValue is zero.")
+}
+
+
+var a = 5
+
+var b = 10
 
 
 let number = 85;
@@ -19,18 +61,12 @@ if (number >= 90){
     console.log(house);
 }
 
-else if(number > 90){
+else if(80 <= number <= 90){
     house = "Grade B";
     console.log(house);
 }
 
-else if(number > 70){
-    house = "Grade C";
-    console.log(house);
-}
-
-
 else{
-    house = "Fail";
+    house = "Grade C";
     console.log(house);
 }
